aabbb4f1f22686a1f4c3734951ce2c9b66c77f19
dodacodec <doougmoraes@gmail.com>
2026-02-24 21:30:05 -0500

    Create catalogs.js

---
+++ src/domain/catalogs.js
@@ -0,0 +1,31 @@
+export const DEFAULT_ACCOUNTS = [
+  { id: "bofa_cc", name: "BofA CC" },
+  { id: "bofa_checking", name: "BofA Checking" },
+  { id: "wells_cc", name: "Wells CC" },
+  { id: "wells_checking", name: "Wells Checking" },
+];
+
+export const DEFAULT_TXN_TYPES = [
+  { id: "expense", name: "Expense" },
+  { id: "income", name: "Income / Refund" },
+  { id: "transfer", name: "Transfer / Payment" },
+];
+
+export const DEFAULT_CATEGORIES = [
+  { id: "food", name: "Food" },
+  { id: "groceries", name: "Groceries" },
+  { id: "dining", name: "Dining Out" },
+  { id: "gas", name: "Gas" },
+  { id: "transport", name: "Transportation" },
+  { id: "house", name: "House" },
+  { id: "subscription", name: "Subscriptions" },
+  { id: "shopping", name: "Shopping" },
+  { id: "health", name: "Health" },
+  { id: "pets", name: "Pets" },
+  { id: "entertainment", name: "Entertainment" },
+  { id: "golf", name: "Golf" },
+  { id: "gifts", name: "Gifts / Donations" },
+  { id: "fees", name: "Fees / Interest" },
+  { id: "uncategorized", name: "Uncategorized (Fix later)" },
+  { id: "convenience", name: "Convenience Store" },
+];
